5fc98d19bf70ebec409a82c68e385e1858265342
ABHYUDAY MISHRA <78090692+2abhyudaymishra@users.noreply.github.com>
2026-07-22 05:55:54 +0530

    Time: 119 ms (23.66%) | Memory: 175.9 MB (8.48%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int ones = 0;
-        for (char c : s)
-            if (c == '1')
-                ones++;
-
-        string t = "1" + s + "1";
-
-        vector<pair<char, int>> runs;
-
-        for (char c : t) {
-            if (runs.empty() || runs.back().first != c)
-                runs.push_back({c, 1});
-            else
-                runs.back().second++;
-        }
-
-        int best = 0;
-
-        for (int i = 1; i + 1 < (int)runs.size(); i++) {
-            if (runs[i].first == '1' &&
-                runs[i - 1].first == '0' &&
-                runs[i + 1].first == '0') {
-
-                best = max(best, runs[i - 1].second + runs[i + 1].second);
+        int oneCount =0;
+        for(auto it: s)if(it=='1')oneCount++;
+        string t = "1" + s +"1";
+        //asper Q first 
+        //convert 1 within zeros 
+        //convert 0 within one
+        //so if we take make number of zeros containing one our prob is solve
+        //eg -> 10011001 ->  10000001 -> 11111111  we directly count max-> 001100
+        vector<pair<char,int>>arr; // count of contigous char 
+        for(auto it: s){
+            //diff char 
+            if(arr.empty() || arr.back().first!=it){
+                arr.push_back({it,1});
+            }else{
+                arr.back().second++;
             }
         }
 
-        return ones + best;
+        int maxTrade=0;
+        for(int i=1; i+1<arr.size();i++){
+            //this whole can be converted
+            if(arr[i-1].first=='0' && arr[i].first=='1' && arr[i+1].first=='0'){
+                maxTrade = max(maxTrade, arr[i-1].second+ arr[i+1].second);//one inise is already considered in one count;
+            }
+        }
+
+
+        return oneCount + maxTrade;
     }
 };
